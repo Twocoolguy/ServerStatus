04938b58ae174bad99ee51295e3f993528d0156b
TurtlesAreHot <37197684+Twocoolguy@users.noreply.github.com>
2021-10-13 15:57:25 -0400

    alert method

--- src/main/java/de/themoep/serverstatus/ServerStatus.java
+++ src/main/java/de/themoep/serverstatus/ServerStatus.java
@@ -1,6 +1,7 @@
 package de.themoep.serverstatus;
 
 import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.config.Configuration;
@@ -107,5 +108,9 @@ public class ServerStatus extends Plugin {
             }
         }
         return string;
+    }
+
+    public static void alert(String message) {
+        ProxyServer.getInstance().getPluginManager().dispatchCommand(ProxyServer.getInstance().getConsole(), "alert " + message);
     }
 }
